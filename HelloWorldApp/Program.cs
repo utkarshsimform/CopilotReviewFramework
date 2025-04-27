e610314eebcdf01234ef8fec099a3dc078f009f1
Utkarsh Prajapati <utkarsh.p@simformsolutions.com>
2025-04-27 19:49:24 +0530

    Test commit

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -4,27 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        string message = "This is never used";
+        string message = "This is never used1";
 
-        string apiKey = "sk-123456789abcdef";
+        string apiKey = "sk-123456789abcdef1";
 
         Console.WriteLine("Hello Utkarsh");
 
         try
         {
-            int num = int.Parse("oops");
+            int num = int.Parse("oops1");
         }
         catch (Exception)
         {
             // ignored
         }
 
-        Console.WriteLine("User: " + "utkarsh" + " logged in.");
+        Console.WriteLine("User: " + "utkarsh1" + " logged in.");
     }
 
-    public static int Add(int a, int b)
+    public static int Add(int a, int c)
     {
-        return a + b;
+        return a + c;
     }
 }
 
